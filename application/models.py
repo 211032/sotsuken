e0a070b8a842fe1c15838d94f375c0fc1c21dbeb
211032 <211032@std.hi-joho.ac.jp>
2024-10-23 13:52:36 +0900

    10/21　講師DB設定（終了）

--- application/models.py
+++ application/models.py
@@ -5,3 +5,11 @@ class Student(models.Model):
     class_name = models.CharField(max_length=100)  # クラス名
     name = models.CharField(max_length=100)  # 学生の名前
     password = models.CharField(max_length=128)  # パスワード
+
+
+class teacher(models.Model):
+    teacher_id = models.AutoField(primary_key=True)  # 講師ID（自動で増加する）
+    name = models.CharField(max_length=100)  # 講師の名前
+    password = models.CharField(max_length=128)  # ハッシュ化されたパスワードを保存
+    romanized_last_name = models.CharField(max_length=100)  # 講師のローマ字の姓
+    roll = models.CharField(max_length=50)  # 役割
