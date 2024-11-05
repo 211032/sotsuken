8fad737caa0eaaa42d749f784f784bdaeda347f4
217003 <231201@std.hi-joho.ac.jp>
2024-11-05 15:39:28 +0900

    databaseの変更

--- application/classroom_checker.py
+++ application/classroom_checker.py
@@ -1,8 +1,9 @@
-from .models import Equipment
+from .models import Equipment, Classroom
 from asgiref.sync import sync_to_async
 
 @sync_to_async
 def get_classroom_name(minor):
     equipment = Equipment.objects.get(minor=minor)
+    classroom_name = Classroom.objects.get(classroom_id=equipment.location_id).classroom_name
 
-    return equipment.location
+    return classroom_name
